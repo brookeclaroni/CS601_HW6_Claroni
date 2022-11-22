dd328d58cbb15f2500a345059ee499c4735f7bbb
Brooke Claroni <brookeclaroni@gwu.edu>
2022-11-21 22:30:13 -0500

    base functionality

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 const { createApp } = Vue
 
 createApp({
+    methods: {
+        getBaristaImage: function () {
+            if (this.baristaHover == true) {
+                return "./images/barista_sunny.jpg"
+            } else {
+                return "./images/barista.jpg"
+            }
+        },
+        getPostalImage: function () {
+            if (this.postalHover == true) {
+                return "./images/postal_sunny.jpg"
+            } else {
+                return "./images/postal.jpg"
+            }
+            
+        },
+        getLionImage: function () {
+            if (this.lionHover == true) {
+                return "./images/lion_sunny.jpg"
+            } else {
+                return "./images/lion.jpg"
+            }
+        },
+        getHotdogImage: function () {
+            if (this.hotdogHover == true) {
+                return "./images/hotdog_sunny.jpg"
+            } else {
+                return "./images/hotdog.jpg"
+            }
+        }
+    },
     data() {
         return {
-            message: 'Hello Vue!'
+            baristaHover: false,
+            postalHover: false,
+            lionHover: false,
+            hotdogHover: false,
+            titleMessage: "Sunny's Careers & Where They Began"
         }
     }
 }).mount('#app')
