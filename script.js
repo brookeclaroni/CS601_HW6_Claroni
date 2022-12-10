e3c9aeaa858dbb6b79af89c91fc050cc121677db
Brooke Claroni <brookeclaroni@gwu.edu>
2022-12-09 22:25:44 -0500

    make code more concise

--- script.js
+++ script.js
@@ -1,44 +1,21 @@
 const { createApp } = Vue
 
 createApp({
-    methods: {
-        getBaristaImage() {
-            if (this.baristaHover == true) {
-                return "./images/barista_sunny.jpg"
-            } else {
-                return "./images/barista.jpg"
-            }
-        },
-        getPostalImage() {
-            if (this.postalHover == true) {
-                return "./images/postal_sunny.jpg"
-            } else {
-                return "./images/postal.jpg"
-            }
-            
-        },
-        getLionImage() {
-            if (this.lionHover == true) {
-                return "./images/lion_sunny.jpg"
-            } else {
-                return "./images/lion.jpg"
-            }
-        },
-        getHotdogImage () {
-            if (this.hotdogHover == true) {
-                return "./images/hotdog_sunny.jpg"
-            } else {
-                return "./images/hotdog.jpg"
-            }
-        }
-    },
     data() {
         return {
-            baristaHover: false,
-            postalHover: false,
-            lionHover: false,
-            hotdogHover: false,
-            titleMessage: "Sunny's Careers & Where They Began"
+          titleMessage: "Sunny's Careers & Where They Began",
+          currentBaristaImage: "./images/barista.jpg",
+          baristaSunny: "./images/barista_sunny.jpg",
+          barista: "./images/barista.jpg",
+          currentPostalImage:"./images/postal.jpg",
+          postalSunny: "./images/postal_sunny.jpg",
+          postal: "./images/postal.jpg",
+          currentLionImage: "./images/lion.jpg",
+          lionSunny: "./images/lion_sunny.jpg",
+          lion: "./images/lion.jpg",
+          currentHotdogImage:"./images/hotdog.jpg",
+          hotdogSunny: "./images/hotdog_sunny.jpg",
+          hotdog: "./images/hotdog.jpg"
         }
     }
 }).mount('#app')
